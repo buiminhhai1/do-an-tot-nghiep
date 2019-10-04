ccb560fcae63051a650deb4b35aaa25677ad9826
buiminhhai1 <ngovietduc20088@gmail.com>
2019-10-04 19:25:50 +0700

    add something

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,11 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
+
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 module.exports = router;
+
